c8e658fb251a68799bf420fc1ba5a7fe285e939b
Haider Sohail <haidersuhail606@gmail.com>
2024-01-24 12:39:58 +0500

    map_operations

---
+++ map_operations.js
@@ -0,0 +1,5 @@
+
+let originalArray = [3, 7, 10];
+let doubledArray = originalArray.map(number => number * 2);
+console.log("Original Array:", originalArray);
+console.log("Doubled Array:", doubledArray);
