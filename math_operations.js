a4f9db73b6b370cf1f9756b40d78dde99e8aa523
Haider Sohail <haidersuhail606@gmail.com>
2024-01-24 12:27:31 +0500

    math_operations

---
+++ math_operations.js
@@ -0,0 +1,7 @@
+
+let squareRootOf25 = Math.sqrt(25);
+console.log("Square root of 25:", squareRootOf25);
+let randomNumBetween1And10 = Math.floor(Math.random() * 10) + 1;
+console.log("Random number between 1 and 10:", randomNumBetween1And10);
+let roundedNumber = Math.round(3.14159);
+console.log("Rounded number:", roundedNumber);
